e2bf516f45b9e3fb2cc102286ddf8ae38acc9782
Lysander <121090088@link.cuhk.edu.cn>
2022-06-03 23:17:10 +0800

    init

---
+++ Welcome/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Hello world!
+
+#include<iostream>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
---
+++ build-CS106-Qt_6_2_4_for_macOS-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Fri Jun 3 21:44:22 2022"
+#define SPL_BUILD_USER "lysander"
+
+#endif
